0a1d740441a3072083d9ccb510e1e98ee097fc15
AkashGP2 <vakash@deloitte.com>
2023-01-20 00:00:23 +0530

    tried adding lambda handler

--- New-CS/Program.cs
+++ New-CS/Program.cs
@@ -1,16 +1,28 @@
 ﻿using Amazon.Lambda.Core;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
+namespace LambdaTest;
 
-namespace New_CS
+public class LambdaHandler 
 {
-    internal class Program
+    public void handleRequest()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello World, Lambda here");
     }
 }
+// namespace New_CS
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
